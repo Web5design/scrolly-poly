82af12575236896f076c16b7dab695c4cfa374df
David DeSandro <david@desandro.com>
2013-04-21 14:40:47 -0400

    initial commit

---
+++ scrolly-poly.js
@@ -0,0 +1,135 @@
+/*!
+ * ScrollyPoly
+ * infinite scrolling
+**/
+
+( function( window ) {
+
+'use strict';
+
+// ----- dependencies ----- //
+
+var eventie = window.eventie;
+var EventEmitter = window.EventEmitter;
+var getSize = window.getSize;
+
+// ----- vars ----- //
+
+var document = window.document;
+var docElem = document.documentElement;
+
+// -------------------------- helpers -------------------------- //
+
+// extend objects
+function extend( a, b ) {
+  for ( var prop in b ) {
+    a[ prop ] = b[ prop ];
+  }
+  return a;
+}
+
+// http://stackoverflow.com/a/384380/182183
+var isElement = ( typeof HTMLElement === 'object' ) ?
+  function isElementDOM2( obj ) {
+    return obj instanceof HTMLElement;
+  } :
+  function isElementQuirky( obj ) {
+    return obj && typeof obj === 'object' &&
+      obj.nodeType === 1 && typeof obj.nodeName === 'string';
+  };
+
+// -------------------------- ScrollyPoly -------------------------- //
+
+function ScrollyPoly( element, options ) {
+  // bail out if not proper element
+  if ( !element || !isElement( element ) ) {
+    if ( console ) {
+      console.error( 'Bad ' + this.settings.namespace + ' element: ' + element );
+    }
+    return;
+  }
+
+  this.element = element;
+
+  // options
+  this.options = extend( {}, this.options );
+  extend( this.options, options );
+
+  // kick it off
+  this._create();
+}
+
+// add event emitter
+extend( ScrollyPoly.prototype, EventEmitter.prototype );
+
+ScrollyPoly.prototype.options = {
+  scroller: window,
+  triggerHeight: 150
+};
+
+ScrollyPoly.prototype._create = function() {
+  if ( this.options.scroller ) {
+    eventie.bind( this.options.scroller, 'scroll', this );
+  }
+};
+
+ScrollyPoly.prototype.ajaxNextPage = function() {
+  console.log('ajax next page');
+};
+
+// -------------------------- events -------------------------- //
+
+// enable event handlers for listeners
+// i.e. scroll -> scroll
+ScrollyPoly.prototype.handleEvent = function( event ) {
+  var method = 'on' + event.type;
+  if ( this[ method ] ) {
+    this[ method ]( event );
+  }
+};
+
+// original debounce by John Hann
+// http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+
+// this fires every resize
+ScrollyPoly.prototype.onscroll = function() {
+  if ( this.scrollTimeout ) {
+    clearTimeout( this.scrollTimeout );
+  }
+
+  var _this = this;
+  function delayed() {
+    _this.scroll();
+    delete _this.scrollTimeout;
+  }
+
+  this.scrollTimeout = setTimeout( delayed, 100 );
+};
+
+// debounced
+ScrollyPoly.prototype.scroll = function() {
+  // do stuff
+
+  var scrollY = window.pageYOffset !== undefined ? window.pageYOffset :
+    // IE8
+    docElem.scrollTop;
+
+  var windowHeight = window.innerHeight || docElem.clientHeight;
+
+  var bodyHeight = getSize( document.body ).height;
+
+  console.log( scrollY + windowHeight, bodyHeight - this.options.triggerHeight );
+
+  if ( scrollY + windowHeight >= bodyHeight - this.options.triggerHeight ) {
+    this.ajaxNextPage();
+  }
+
+};
+
+
+
+// --------------------------  -------------------------- //
+
+window.ScrollyPoly = ScrollyPoly;
+
+})( window );
